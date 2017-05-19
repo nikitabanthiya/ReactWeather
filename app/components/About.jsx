f6ecc3f9a59d8aebdcaa7c9ef7766b35ebea01cd
Nikita <nikitabanthiya14@gmail.com>
2017-05-19 10:11:03 +0530

    style nav bar

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,13 @@ var React = require('react');
 var About = (props)=> {
   return (
     <div>
-      <h2>About</h2>
-      <p>this is About</p>
+      <h1 classname="text-venter">About</h1>
+      <p>This is weather Application,built on react .Webpack Developer</p>
+      <p>Here are some tools I used</p>
+      <ul>
+      <li><a href="https://facebook.github.io/react">React</a>Jabascript framework</li>
+      </ul>
+      <li><a href="http://openweathermap.org">Open weather Map</a>Open weather Map to search for weather data by city name.</li>
     </div>
   );
 }
